41bd1c92fd88436d9a2724ff9595e926c1d439eb
nusratjahan1873 <nusratjahan322a@gmail.com>
2026-03-11 21:53:38 +0600

    added the filter

--- app.js
+++ app.js
@@ -28,3 +28,48 @@ const loadIssues = ()=>{
         manageSpinner(false);
     });
 };
+
+
+// display issues
+
+const displayIssues=(issues)=>{
+   const  container= document.getElementById("issues-container");
+   container.innerHTML="";
+   issues.forEach(issue =>{
+    const div = document.createElement("div");
+
+   let borderColor = issue.status === "open"
+    ? "border-green-500" : "border-purple-500"; 
+    div.innerHTML = ` <div onclick="openModal('${issue.title}','${issue.description}')" class="border-t-4 ${borderColor} bg-white p-4 rounded shadow cursor-pointer">
+     <h2 class="font-bold">${issue.title}</h2>
+     <p class="text-sm text-gray-500 mt-2"> ${issue.description.slice(0,80)}... </p> 
+     <p class="text-xs mt-3"> By ${issue.author} </p>
+      </div> `;
+
+
+      container.appendChild(div);
+   });  
+};
+
+
+// filter issue
+
+const filterIssues =(status)=>{
+    manageSpinner(true);
+
+    fetch(API)
+    .then(res => res.json())
+    .then(data=>{
+
+
+        let issues = data.data;
+         if(status !== "all"){
+             issues = issues.filter(issue => issue.status === status);
+             }
+              displayIssues(issues); 
+
+              manageSpinner(false);
+    });
+};
+
+
